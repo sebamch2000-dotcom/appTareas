85e297a0b27e2e58caa4137ea800b895d0e66acb
chanta <sevenxfake@gmail.com>
2026-09-07 15:44:48 -0500

    entidades

---
+++ app/src/main/java/entidades/Usuario.java
@@ -0,0 +1,32 @@
+package entidades;
+
+public class Usuario {
+    private int id;
+    private String nombre;
+    private String correo;
+    private String password;
+
+    // Constructor vacío obligatorio
+    public Usuario() {}
+
+    // Constructor completo
+    public Usuario(int id, String nombre, String correo, String password) {
+        this.id = id;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.password = password;
+    }
+
+    // Métodos Getters y Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getCorreo() { return correo; }
+    public void setCorreo(String correo) { this.correo = correo; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+}
